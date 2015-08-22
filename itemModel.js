8b04f5fb4de891eff04f2b4a6bd2a69feb11c9f6
Xian <tmax898@gmail.com>
2015-08-22 01:11:15 -0500

    adding in issue comment

--- itemModel.js
+++ itemModel.js
@@ -1,4 +1,8 @@
-    var Item = Backbone.Model.extend({
+/*
+This does not work in chrome because it does not like the 127.0.0.1
+or the localhost url root and will throw an error.
+*/
+var Item = Backbone.Model.extend({
         //Create a model to hold friend atribute
         name: null,
         ap: null,
