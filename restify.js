834b031136bd86857a2b53b995396970bae46ca6
Xian <tmax898@gmail.com>
2015-08-24 12:37:56 -0500

    allowing CORS in restify

--- restify.js
+++ restify.js
@@ -30,6 +30,13 @@ function fetchAllItems(req, res, next) {
 }
 
 var server = restify.createServer();
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
