9e017e73823ca0aef99d6d5d55845376aab4404c
Xian <tmax898@gmail.com>
2015-08-23 19:51:34 -0500

    removing hard coding....

--- js/itemModel.js
+++ js/itemModel.js
@@ -5,23 +5,8 @@ or the localhost url root and will throw an error.
 var ItemModel = Backbone.Model.extend({
     //Skipping a backend object model, we'll do the scrubbing here
     //and add from the fetch result, populate attr we care about.
-    name: null,
-    description : "",
-    ap: null,
-    ad: null,
-    id: 0,
     url: function () {
         return 'http://127.0.0.1:8080/fetchItemById/' + this.id
-    },
-    //To do our scrubbing we'll parse out the response and instead
-    // of getting it as an 'attriubtes' value, we'll parse through
-    // it and assign it back to the base set of attr we define.
-    parse : function (response){
-        console.log(response);
-        this.name = response.name;
-        this.description = response.sanitzedDescription;
-
-
     }
 });
 
--- restify.js
+++ restify.js
@@ -4,16 +4,10 @@ var mongojs = require("mongojs");
 var db = mongojs("blackmarketdb");
 var itemCollection = db.collection("items");
 
-function respond(req, res, next) {
-    res.send('hello ' + req.params.name);
-    next();
-}
-
 function fetchItemById(req, res, next) {
     var id = req.params.id;
-
     itemCollection.find({
-        id: 3165
+        id: parseInt(id)
     }, function (err, response) {
         res.writeHead(200, {
             'Content-Type': 'application/json; charset=utf-8'
@@ -40,10 +34,8 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/hello/:name', respond);
 server.get('/fetchItemById/:id', fetchItemById);
 server.get('/fetchAllItems', fetchAllItems);
-server.head('/hello/:name', respond);
 
 server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
