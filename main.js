dec4980d625834e2b9bbe15b92943b80f82effef
Wesley <wesleyrogers09@gmail.com>
2015-08-24 21:05:05 -0500

    Pushing out some basic chart stuff, not working at the moment... :truck:

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
-        $("#filter").load("templates/filter.html", function(){
-            $("#filter-accordion").accordion({heightStyle : "content"});
+    console.log("doc ready!");
+    $("#filter").load("templates/filter.html", function () {
+        $("#filter-accordion").accordion({
+            heightStyle: "content"
         });
+    });
 
-    }
-);
+    console.log("Loading up our items");
+
+    console.log("Creating the charts!");
+    var ctx = $("#firstChart")[0].getContext("2d");
+    console.log(ctx);
+
+    //Not sure this one does anything usefull, just took off what I had in orionhub
+    var mynewChart = new Chart(ctx);
+    // this is most likely the chart that should be one that is viewed, since it has data...
+    var lineChart = Chart(ctx).Line(orionData, {
+        bezierCurve: true
+    });
+});
