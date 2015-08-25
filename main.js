78c4280b559ece8b3be3dd0c6ca04ffba70c4bb3
rogerwes <wesleyrogers09@gmail.com>
2015-08-25 08:59:13 -0500

    Removed merge bugs

--- main.js
+++ main.js
@@ -13,26 +13,14 @@ $(document).ready(function () {
     var ctx = $("#firstChart").get(0).getContext("2d");
     console.log(ctx);
 
-<<<<<<< HEAD
     // First chart
     var lineChart = new Chart(ctx).Line(orionData);
     var ctx = $("#secondChart").get(0).getContext("2d");
     var lineChart2 = new Chart(ctx).Line(simpleData);
 
-
     //chart defaults:
-
     Chart.defaults.global = {
         showScale: true,
         scaleFontColor: "#555"
     };
-=======
-    //Not sure this one does anything usefull, just took off what I had in orionhub
-    var mynewChart = new Chart(ctx);
-    // this is most likely the chart that should be one that is viewed, since it has data...
-    console.log(data);
-    var lineChart = new Chart(ctx).Line(data, {
-        bezierCurve: true
-    });
->>>>>>> origin/master
 });
