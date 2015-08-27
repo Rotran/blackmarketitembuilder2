7a0a3c4be016c9d68884d1dddeb3b685eb4bf32c
Xian <tmax898@gmail.com>
2015-08-26 20:43:36 -0500

    search functionality

---
+++ js/itemSearch.js
@@ -0,0 +1,19 @@
+$(function(){
+    $.widget("blackmarket.itemSearch", {
+        options:{
+
+        },
+        _create : function(){
+            var itemSearch = this;
+            $("#search-button").on('click', function(){
+                console.log("CLICKING");
+                var searchText = $("#search-text").val();
+                console.log(searchText);
+                itemSearch._search(searchText);
+            });
+        },
+        _search : function(searchText){
+            $("#item-result").itemResults('filterDisplayItems',searchText);
+        }
+    });
+});
