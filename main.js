3beaec793c26d168a8a1252bf72b214c64a602eb
Xian <tmax898@gmail.com>
2015-08-27 21:16:28 -0500

    design changes to draggables and cleaned up droppable

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ $(document).ready(function () {
         success: function (models, response, options) {
             console.log("MODELS", models);
             allItems = models.models;
-            $("#item-result").itemResults({allItems : allItems});
+            $("#item-result").itemResults({
+                allItems: allItems
+            });
             $(document).trigger("all-items-loaded");
             $("#search-button").itemSearch();
             $("#item-filter").filterItems();
@@ -22,64 +24,37 @@ $(document).ready(function () {
         });
     });
 
-    $("#drop-items").load("templates/dropped.html", function(){
-        $("#startGame").droppable({
+    function createDroppable(divId) {
+        $("#" + divId).droppable({
             activeClass: "ui-state-default",
-			hoverClass: "ui-state-hover",
-			accept: ":not(.ui-sortable-helper)",
-			drop: function( event, ui ) {
-				$( this ).find( ".placeholder" ).remove();
+            hoverClass: "ui-state-hover",
+            accept: ":not(.ui-sortable-helper)",
+            drop: function (event, ui) {
+                $(this).find(".placeholder").remove();
                 var imageSource = ui.draggable.find("img").attr("src");
                 var imageId = ui.draggable.find("img").attr("id");
-                var template = "<li id="+imageId+"><img class=item-icon src=" + imageSource + "></li>";
-                $("#startGameList").append(template);
+                var template = "<li>" + ui.draggable.html().toString() + "</li>";
+                $("#"+divId+"List").append(template);
                 //perhaps we have a widget which shows the icon + gold cost??
                 generateDataFromDrop(ui.draggable);
-                var idd = ui.draggable.find("img").attr("id") ;
+                var idd = ui.draggable.find("img").attr("id");
                 console.log(idd);
-            //TODO: after we drop, we should add the item
-            // to the list
+                //TODO: after we drop, we should add the item
+                // to the list
             }
         }).sortable({
             items: "li:not(.placeholder)",
-            sort: function(){
+            sort: function () {
                 $(this).removeClass("ui-state-default");
             }
         });
-        //midGame
-        $("#midGame").droppable({
-            activeClass: "ui-state-default",
-			hoverClass: "ui-state-hover",
-			accept: ":not(.ui-sortable-helper)",
-			drop: function( event, ui ) {
-				$( this ).find( ".placeholder" ).remove();
-				var imageSource = ui.draggable.find("img").attr("src");
-                var template = "<li><img class=item-icon src=" + imageSource + "></li>";
-                $("#midGameList").append(template);
-            }
-        }).sortable({
-            items: "li:not(.placeholder)",
-            sort: function(){
-                $(this).removeClass("ui-state-default");
-            }
-        });
-        //endGame
-        $("#endGame").droppable({
-            activeClass: "ui-state-default",
-			hoverClass: "ui-state-hover",
-			accept: ":not(.ui-sortable-helper)",
-			drop: function( event, ui ) {
-				$( this ).find( ".placeholder" ).remove();
-				var imageSource = ui.draggable.find("img").attr("src");
-                var template = "<li><img class=item-icon src=" + imageSource + "></li>";
-                $("#endGameList").append(template);
-            }
-        }).sortable({
-            items: "li:not(.placeholder)",
-            sort: function(){
-                $(this).removeClass("ui-state-default");
-            }
-        });
+    }
+
+    $("#drop-items").load("templates/dropped.html", function () {
+        createDroppable("startGame");
+        createDroppable("midGame");
+        createDroppable("endGame");
+
     });
 
     var ctx = $("#firstChart").get(0).getContext("2d");
