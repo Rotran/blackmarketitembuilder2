f94bef9bf5ccae1919747fa3db996bdf23257435
unknown <tmax898@gmail.com>
2015-10-14 14:16:37 -0500

    making the model fetch non-hard coded

--- js/models/itemModel.js
+++ js/models/itemModel.js
@@ -1,17 +1,20 @@
-define(['backbone'],function(Backbone){
+define(['backbone'], function (Backbone)
+{
 
-/*
-This does not work in chrome because it does not like the 127.0.0.1
-or the localhost url root and will throw an error.
-*/
-var ItemModel = Backbone.Model.extend({
-    //Skipping a backend object model, we'll do the scrubbing here
-    //and add from the fetch result, populate attr we care about.
-    url: function () {
-        return 'http://blackmarket.rotran.io:8080/fetchItemById/' + this.id
-    }
-});
+    /*
+    This does not work in chrome because it does not like the 127.0.0.1
+    or the localhost url root and will throw an error.
+    */
+    var ItemModel = Backbone.Model.extend(
+    {
+        //Skipping a backend object model, we'll do the scrubbing here
+        //and add from the fetch result, populate attr we care about.
+        url: function ()
+        {
+            return window.location.hostname ':8080/fetchItemById/' + this.id
+        }
+    });
 
-return ItemModel;
+    return ItemModel;
 
 });
